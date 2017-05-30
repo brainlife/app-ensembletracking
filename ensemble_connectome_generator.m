149fa2476c553545d7f1b664d99c924257ed36f0
kitchell <lindseykitchell21@gmail.com>
2017-05-30 14:45:29 -0400

    update

--- ensemble_connectome_generator.m
+++ ensemble_connectome_generator.m
@@ -21,16 +21,16 @@ fg = fgRead('wm_tensor.tck');
 
 % Subsample the fascicles 500,000 are too many (eliminate this step in the future by reducing the number of fascicles you 
 % track, track 60,000 fascicles max for 13 tractography methods and 100,000x70 data points).
-fgIdx = randsample(1:length(fg.fibers), 60000);
-fg = fgExtract(fg,fgIdx,'keep');
+%fgIdx = randsample(1:length(fg.fibers), 60000);
+%fg = fgExtract(fg,fgIdx,'keep');
 
 % CSD-based tracking. Load one at the time.
 for ilm = 1:length(lmaxparam)
     for isp = 1:length(streamprob)
         
         fg_tmp = fgRead(fullfile(sprintf('csd_lmax%s_wm_SD_%s.tck',lmaxparam{ilm},streamprob{isp})));
-        fgIdx = randsample(1:length(fg_tmp.fibers),60000);
-        fg_tmp = fgExtract(fg_tmp,fgIdx,'keep');
+        %fgIdx = randsample(1:length(fg_tmp.fibers),60000);
+        %fg_tmp = fgExtract(fg_tmp,fgIdx,'keep');
 
         % Merge the new fiber group with the original fiber group.
         fg = fgMerge(fg,fg_tmp);
