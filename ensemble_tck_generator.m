f929e572f414b4dffd556462ed5bba94c91c71af
kitchell <lindseykitchell21@gmail.com>
2017-06-29 14:28:05 -0400

    edit to use tck

---
+++ ensemble_tck_generator.m
@@ -0,0 +1,34 @@
+function ensemble_tck_generator()
+
+
+switch getenv('ENV')
+case 'IUHPC'
+        disp('loading paths (HPC)')
+        addpath(genpath('/N/u/hayashis/BigRed2/git/vistasoft'))
+case 'VM'
+        disp('loading paths (VM)')
+  	addpath(genpath('/usr/local/vistasoft'))
+end
+
+% find all the .tck files
+ens = dir('*.tck');
+% pull all the file names
+ens_names = {ens.name};
+
+% loop over and import all the ensemble connectomes
+%ens_fg = fgCreate('name', 'ens_fg');
+ens_fg = fgRead(char(ens_names(1)));
+
+for ii = 2:length(ens_names)
+    
+    tfg = dtiImportFibersMrtrix(char(ens_names(ii)));
+       
+    % append the new streamlines to the fiber group
+    ens_fg.fibers = [ ens_fg.fibers; tfg.fibers ];
+    
+end
+% save out
+
+dtiExportFibersMrtrix(ens_fg, 'track.tck')
+
+end
