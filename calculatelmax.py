c8b3dc9ec74c17ac0624118af6a4efdc66968d02
Lindsey Kitchell <kitchell@indiana.edu>
2017-08-04 20:25:17 -0400

    add python to calc max lmax

---
+++ calculatelmax.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  4 20:06:19 2017
+
+@author: lindseykitchell
+"""
+
+#get non0 bvals
+f = open('dwi_aligned_trilin_noMEC.bvals', 'r')
+line = f.readline().strip().replace(",", " ")
+bvals = line.split(" ")
+bvals_non0 = filter(lambda v: v != "0", bvals)
+count=len(bvals_non0);
+
+#calculate max lmax (6->2, 15->4, 28->6, etc..)
+lmax=0
+while (lmax+3)*(lmax+4)/2 <= count:
+       lmax+=2
+
+print "lmax should be ", lmax, "for bvals:", count
+
+outfile = open('lmax.txt', 'w')
+outfile.write(str(lmax))
+outfile.close()
