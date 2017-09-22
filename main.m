cbcc201106ed000704616c038a378b5c7aada4f2
Lindsey Kitchell <lindseykitchell21@gmail.com>
2017-09-22 19:50:20 -0400

    Update main.m

--- main.m
+++ main.m
@@ -8,19 +8,22 @@ end
 switch getenv('ENV')
 case 'IUHPC'
         disp('loading paths (HPC)')
-        addpath(genpath('/N/u/hayashis/BigRed2/git/vistasoft'))
-        addpath(genpath('/N/u/hayashis/BigRed2/git/jsonlab'))
+        addpath(genpath('/N/u/brlife/git/vistasoft'))
+        %addpath(genpath('/N/u/brlife/git/jsonlab'))
 case 'VM'
         disp('loading paths (VM)')
-  	addpath(genpath('/usr/local/jsonlab'))
+  	%addpath(genpath('/usr/local/jsonlab'))
 	addpath(genpath('/usr/local/vistasoft'))
 end
 
-config = loadjson('config.json');
+%config = loadjson('config.json');
 
+bvecs = getenv('BVECS');
+bvals = getenv('BVALS');
 %% Create an MRTRIX .b file from the bvals/bvecs of the shell chosen to run
 out   = 'grad.b';
-mrtrix_bfileFromBvecs(config.bvecs, config.bvals, out);
+%mrtrix_bfileFromBvecs(config.bvecs, config.bvals, out);
+mrtrix_bfileFromBvecs(bvecs, bvals, out);
 
 % load my own config.json
 [ out ] = make_wm_mask(config);
