dfb89cbc38899d9e6f19f79cf8ab64321b10f153
Lindsey Kitchell <lindseykitchell21@gmail.com>
2017-09-22 20:04:16 -0400

    Update main.m

--- main.m
+++ main.m
@@ -9,14 +9,14 @@ switch getenv('ENV')
 case 'IUHPC'
         disp('loading paths (HPC)')
         addpath(genpath('/N/u/brlife/git/vistasoft'))
-        %addpath(genpath('/N/u/brlife/git/jsonlab'))
+        addpath(genpath('/N/u/brlife/git/jsonlab'))
 case 'VM'
         disp('loading paths (VM)')
-  	%addpath(genpath('/usr/local/jsonlab'))
+  	addpath(genpath('/usr/local/jsonlab'))
 	addpath(genpath('/usr/local/vistasoft'))
 end
 
-%config = loadjson('config.json');
+config = loadjson('config.json');
 
 bvecs = getenv('BVECS');
 bvals = getenv('BVALS');
