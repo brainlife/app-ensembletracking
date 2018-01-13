781da71dfa31fa770c166227a2a7056629953764
kitchell <lindseykitchell21@gmail.com>
2018-01-13 17:02:51 -0500

    fix tck

--- ensemble_tck_generator.m
+++ ensemble_tck_generator.m
@@ -15,21 +15,37 @@ ens = dir('*.tck');
 % pull all the file names
 ens_names = {ens.name};
 
-% loop over and import all the ensemble connectomes
-%ens_fg = fgCreate('name', 'ens_fg');
-ens_fg = dtiImportFibersMrtrix(char(ens_names(1)), .5)
-%ens_fg = fgRead(char(ens_names(1)));
 
+tracks = read_mrtrix_tracks(char(ens_names(1)));
+all_tracks = tracks;
+all_tracks.count = str2double(all_tracks.count);
 for ii = 2:length(ens_names)
-    
-    tfg = dtiImportFibersMrtrix(char(ens_names(ii)), .5);
-       
-    % append the new streamlines to the fiber group
-    ens_fg.fibers = [ ens_fg.fibers; tfg.fibers ];
-    
+    temptracks = read_mrtrix_tracks(char(ens_names(ii)));
+    all_tracks.count = all_tracks.count + str2double(temptracks.count);
+    all_tracks.data = [all_tracks.data temptracks.data];
 end
-% save out
 
-dtiExportFibersMrtrix(ens_fg, 'track.tck')
+all_tracks.count = num2str(all_tracks.count);
+all_tracks.roi = '';
+write_mrtrix_tracks(all_tracks, 'track.tck')
+
+
+% 
+% % loop over and import all the ensemble connectomes
+% %ens_fg = fgCreate('name', 'ens_fg');
+% ens_fg = dtiImportFibersMrtrix(char(ens_names(1)), .5)
+% %ens_fg = fgRead(char(ens_names(1)));
+% 
+% for ii = 2:length(ens_names)
+%     
+%     tfg = dtiImportFibersMrtrix(char(ens_names(ii)), .5);
+%        
+%     % append the new streamlines to the fiber group
+%     ens_fg.fibers = [ ens_fg.fibers; tfg.fibers ];
+%     
+% end
+% % save out
+% 
+% dtiExportFibersMrtrix(ens_fg, 'track.tck')
 
 end
